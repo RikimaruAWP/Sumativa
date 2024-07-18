72bb290e897a33cf3bd0548a355f281a9a0add2b
saul <sacorvacho@hotmail>
2024-07-17 22:43:33 -0400

    2

--- src/components/Nota.jsx
+++ src/components/Nota.jsx
@@ -1,20 +1,22 @@
+import './Nota.css';
+
 
 export function Nota(){
 
-
-
     return(
-        <div>
-            <div className="titulo">
-                <h1>Post It Simulator!</h1>
-            </div>
-            <div className="">
-                <input placeholder="Titulo" className="TituloPost" type="text" />
-                <input placeholder="Descripcion" className="DescripcionPost" type="text" />
-                <input type="checkbox" className="m-2" id="checkbox" />
-                <label className="m-2">Importante!</label>
-            </div>
-
+        <div className='app'>
+            <header className="Header-app">
+                <div className="titulo">
+                    <h1>Post It Simulator!</h1>
+                </div>
+                <div className="">
+                    <input placeholder="Titulo" className="TituloPost" type="text" />
+                    <input placeholder="Descripcion" className="DescripcionPost" type="text" />
+                    <input type="checkbox" className="m-2" id="checkbox" />
+                    <label className="m-2">Importante!</label>
+                    <button className="btn btn-success m-2">AGREGAR</button>
+                </div>
+            </header>
         </div>
     )
 }
